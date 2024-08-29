5d7f8442cf76c38120d9a90abea06c19f4adb805
Nicholas Deluca <143479294+NicholasRagan@users.noreply.github.com>
2024-08-28 22:32:13 -0800

    initial commit
    just an initial commit

---
+++ travel_planner.py
@@ -0,0 +1,20 @@
+def trip_planner_welcome(name):
+  print('Welcome to tripplanner v1.0 ' + name)
+
+trip_planner_welcome('nicholas')
+
+def estimated_time_rounded(estimated_time):
+  rounded_time = round(estimated_time)
+  return rounded_time
+
+estimate = estimated_time_rounded(2.5)
+
+def destination_setup(origin, destination, estimated_time, mode_of_transport='car'):
+  print('Your trip starts off in ' + origin)
+  print('And you are traveling to ' + destination)
+  print('You will be traveling by ' + mode_of_transport)
+  print('It will take approximately ' + str(estimate) + ' hours')
+destination_setup('ak', 'cali', 3)
+
+
+#simply just makes play on simple functions and returns with multiple values.
